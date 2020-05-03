495f75f75c2cfc9e5eb98727e181b7c9222dadc9
sana37 <yusakinatadaka@gmail.com>
2020-05-04 02:57:41 +0900

    テーブル設計を追加

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,7 +1,45 @@
--- TODO: とりあえずuserテーブルだけ書いておいたよー。ここに追記する形でDDL定義してくれたら嬉しい！
 CREATE TABLE users (
-    id   int          auto_increment PRIMARY KEY ,
-    uid  VARCHAR(256) NOT NULL,
-    name VARCHAR(100) NOT NULL
+    id      INT            AUTO_INCREMENT PRIMARY KEY,
+    uid     VARCHAR(256)   NOT NULL,
+    name    VARCHAR(100)   NOT NULL
 );
 CREATE INDEX idx_users ON users(uid);
+
+CREATE TABLE challenge_themes (
+    id              INT             AUTO_INCREMENT PRIMARY KEY,
+    title           VARCHAR(100)    NOT NULL,
+    description     VARCHAR(1024)   NOT NULL,
+    user_id         INT             NOT NULL,
+    recordable      TINYINT(1)      NOT NULL,
+    is_int          TINYINT(1),
+    unit            VARCHAR(64),
+    ranking_type    ENUM('ASC', 'DESC', 'FAV'),
+    created_at      DATETIME        NOT NULL
+);
+
+CREATE TABLE challenge_records (
+    id                  INT             AUTO_INCREMENT PRIMARY KEY,
+    content             VARCHAR(512)    NOT NULL,
+    comment             VARCHAR(1024)   NOT NULL,
+    challenge_theme_id  INT             NOT NULL,
+    user_id             INT             NOT NULL,
+    record              FLOAT,
+    created_at          DATETIME        NOT NULL
+);
+
+CREATE TABLE favorites (
+    id                  INT     AUTO_INCREMENT PRIMARY KEY,
+    user_id             INT     NOT NULL,
+    challenge_record_id INT     NOT NULL
+);
+
+CREATE TABLE tags (
+    id      INT             AUTO_INCREMENT PRIMARY KEY,
+    text    VARCHAR(128)    NOT NULL
+);
+
+CREATE TABLE theme_tag_relation (
+    id                  INT     AUTO_INCREMENT PRIMARY KEY,
+    challenge_theme_id  INT     NOT NULL,
+    tag_id              INT     NOT NULL
+);
