89f0c503cc95351741b8e191e32897c58b308816
sana37 <yusakinatadaka@gmail.com>
2020-05-05 18:24:41 +0900

    #7 #8 レスポンス用の構造体、シリアライザを実装 (#12)
    * レスポンスJSON用の構造体を定義
    
    * GET user API を追加
    
    * エラーを修正
    
    ポインタでわたす
    
    * チャレンジテーマ、レコードのレスポンスを修正

---
+++ api/response/challenge_theme.go
@@ -0,0 +1,33 @@
+package response
+
+import (
+	"barcelonaZoo/pkg/model"
+
+	"github.com/volatiletech/null/v8"
+)
+
+type ChallengeTheme struct {
+	Id          int         `json:"id"`
+	Title       string      `json:"title"`
+	Description string      `json:"description"`
+	User        User        `json:"user"`
+	Recordable  bool        `json:"recordable"`
+	IsInt       null.Bool   `json:"is_int"`
+	Unit        null.String `json:"unit"`
+	RankingType null.Int8   `json:"rankint_type"`
+	CreatedAt   string      `json:"created_at"`
+}
+
+func SerializeChallengeTheme(t *model.ChallengeTheme, u *model.User) *ChallengeTheme {
+	return &ChallengeTheme{
+		Id:          t.ID,
+		Title:       t.Title,
+		Description: t.Description,
+		User:        *SerializeUser(u),
+		Recordable:  t.Recordable,
+		IsInt:       t.IsInt,
+		Unit:        t.Unit,
+		RankingType: t.RankingType,
+		CreatedAt:   t.CreatedAt.Format("2000-01-01 00:00:00"),
+	}
+}
--- pkg/service/user/service.go
+++ pkg/service/user/service.go
@@ -1,10 +1,12 @@
 package user
 
 import (
+	"barcelonaZoo/api/response"
 	"barcelonaZoo/db"
 	"barcelonaZoo/pkg/model"
 	"context"
 
+	"github.com/gin-gonic/gin"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
@@ -19,4 +21,13 @@ func CreateNewUser(ctx context.Context, uid, name string) error {
 	}
 
 	return nil
+}
+
+func GetUser(ctx *gin.Context, id int) (*response.User, error) {
+	u, err := model.FindUser(ctx, db.DB, id)
+	if err != nil {
+		return &response.User{}, err
+	}
+
+	return response.SerializeUser(u), nil
 }
